d1476bb3dfdbfe5c392cb7ada4a7a5afb41bcb78
dtb <dtb@pc-dtb>
2017-01-22 17:13:53 +0100

    Add RestController & some DB functionalities

--- src/main/java/dtb/api/ProductApiApplication.java
+++ src/main/java/dtb/api/ProductApiApplication.java
@@ -1,12 +1,22 @@
 package dtb.api;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	    registration.addUrlMappings("/console/*");
+	    return registration;
 	}
 }
